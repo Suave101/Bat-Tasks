2b695187ede1439700a85eff2c95a10c79213148
Suave101 <57422635+Suave101@users.noreply.github.com>
2020-07-29 10:09:12 -0500

    Create etcinfo.bash
    Shows contents of the passwd file and the shadow file.

---
+++ Cyberpatriot/Bash/etcinfo.bash
@@ -0,0 +1,7 @@
+echo The following is the contents of the passwd file:
+cat /etc/passwd
+echo -----------------------------------------------------------------------------------------
+sleep 2
+echo The following is the contents of the shadow file:
+cat /etc/shadow
+echo -----------------------------------------------------------------------------------------
